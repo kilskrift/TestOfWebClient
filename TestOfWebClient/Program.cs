ba72b313ed57439c1b18ca52a1a1ed462308653c
Kristian G. Madsen <kgm@kgm.cc>
2016-11-29 13:55:29 +0100

    Can perform a well-formed request to RainMan service

--- TestOfWebClient/Program.cs
+++ TestOfWebClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,23 @@ namespace TestOfWebClient
     {
         static void Main(string[] args)
         {
+            string rainManURI = "http://rainman.leanpoker.org/rank";
+            var webClient = new WebClient();
+            var data = "[{ \"rank\":\"5\",\"suit\":\"diamonds\"} ]";
+
+            webClient.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(rainManURI);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            string postData = "cards=" + data;
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] byte1 = encoding.GetBytes(postData);
+            request.ContentLength = byte1.Length;
+            Stream newStream = request.GetRequestStream();
+            newStream.Write(byte1, 0, byte1.Length);            
+            newStream.Close();
         }
     }
 }
